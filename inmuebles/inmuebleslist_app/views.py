37697f9cdeae5ac60f481a2298e41b158d256888
Henry <hmorenosifontes@gmail.com>
2024-02-06 21:40:57 -0400

    first commit

--- inmuebles/inmuebleslist_app/views.py
+++ inmuebles/inmuebleslist_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from inmuebleslist_app.models import Inmueble
+from django.http import JsonResponse
 # Create your views here.
+def inmueble_list(request):
+    inmuebles = Inmueble.objects.all()
+    data = {
+        
+    'inmuebles': list(inmuebles.values())
+        }  # Se pasa los valores de la tabla a un diccionario
+    
+    return JsonResponse(data)
